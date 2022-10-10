8c8673b8aa914099e544b4a9204d2c08ba59bd3d
Documenter.jl <documenter@juliadocs.github.io>
2022-10-10 04:30:54 +0000

    build based on defaeaa

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NewPkg","category":"page"},{"location":"#NewPkg","page":"Home","title":"NewPkg","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NewPkg.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NewPkg]","category":"page"}]
+}
